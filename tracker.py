efdae02b42e3755d7e7923ff9868294ea1fe38b4
Musa <tmusa@iastate.edu>
2018-08-23 14:45:25 -0500

    First iteration: Tracks continuous stream of manga submissions and sends alerts to a user about new chapters they subscribed to.

---
+++ tracker.py
@@ -0,0 +1,47 @@
+import praw
+import config
+
+MESSAGE_TEMPLATE = 'MangaTrackerBot found a new chapter \n {title} \n {link}'
+
+
+def main():
+    stream, redditor, reddit = setup()
+    process_stream(stream, redditor)
+
+
+def setup():
+    reddit = praw.Reddit(client_id=config.CLIENT_ID,
+                         client_secret=config.CLIENT_SECRET,
+                         password=config.PASSWORD,
+                         user_agent=config.USER_AGENT,
+                         username= config.USERNAME)
+
+    subreddit = reddit.subreddit('manga')
+    redditor = reddit.redditor(config.REDDITOR)
+    stream = subreddit.stream.submissions()
+
+    return stream, redditor, reddit
+
+
+def process_stream(stream, redditor):
+    for submission in stream:
+        title = submission.title
+
+        if 'DISC' not in title.upper():
+            return
+
+        manga = config.MANGA_LIST
+        hype_title = title.upper()
+
+        for manga_title in manga:
+            if manga_title in hype_title:
+                send_alert(submission, redditor)
+
+
+def send_alert(submission, redditor):
+    redditor.message('MangaTrackerBot Alert',
+                     MESSAGE_TEMPLATE.format(title=submission.title, link=submission.shortlink))
+
+
+if __name__ == '__main__':
+    main()
