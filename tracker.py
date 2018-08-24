3dc28faed9f50532789f7e145d343031a9a01352
tmusa <tmusa@iastate.edu>
2018-08-24 00:02:46 -0500

    Doing a few things: adding .gitignore to repo. Modified existing code such that manga list isn't a fix list of one user's titles, but a list of manga objects with users subscribed to them.

--- tracker.py
+++ tracker.py
@@ -1,26 +1,31 @@
 import praw
 import config
+import MangaListHelper as mlh
 
 MESSAGE_TEMPLATE = 'MangaTrackerBot found a new chapter \n {title} \n {link}'
 
 
 def main():
     stream, redditor, reddit = setup()
-    process_stream(stream, redditor)
+    for submission in stream:
+        process_stream(submission, redditor)
 
 
 def setup():
+    # print(config.MANGA_LIST)
+    mlh.main()
+    # print(config.MANGA_LIST)
+
     reddit = praw.Reddit(client_id=config.CLIENT_ID,
                          client_secret=config.CLIENT_SECRET,
                          password=config.PASSWORD,
                          user_agent=config.USER_AGENT,
-                         username= config.USERNAME)
+                         username=config.USERNAME)
 
     subreddit = reddit.subreddit('manga')
     redditor = reddit.redditor(config.REDDITOR)
     stream = subreddit.stream.submissions()
-    for submission in stream:
-        process_stream(submission, redditor)
+    return stream, redditor, reddit
 
 
 def process_stream(submission, redditor):
@@ -31,15 +36,18 @@ def process_stream(submission, redditor):
 
         manga = config.MANGA_LIST
         hype_title = title.upper()
-
-        for manga_title in manga:
+        print(title)
+        for m in manga:
+            manga_title = m.title
             if manga_title in hype_title:
                 send_alert(submission, redditor)
+                # print("alert {}".format(manga_title))
 
 
 def send_alert(submission, redditor):
-    redditor.message('MangaTrackerBot Alert',
-                     MESSAGE_TEMPLATE.format(title=submission.title, link=submission.shortlink))
+    msg = MESSAGE_TEMPLATE.format(title=submission.title, link=submission.shortlink)
+    redditor.message('MangaTrackerBot Alert', msg)
+    # print(msg)
 
 
 if __name__ == '__main__':
